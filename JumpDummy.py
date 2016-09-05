8ba13b06c2316bb9118fcebfba17ee215202114a
ShrykeWindgrace <shryke.windgrace@gmail.com>
2016-09-06 00:10:19 +0200

    Separate files for different commands

--- JumpDummy.py
+++ JumpDummy.py
@@ -11,39 +11,3 @@ class JumpToDummyCommand(sublime_plugin.TextCommand):
             self.view.replace(edit, where, cont)  # replace that string in the original text
             self.view.sel().clear()  # clear selection and jump to the place where the dummy used to be
             self.view.sel().add(where.begin())  # the only difficult point here - what to do with multiple selections
-
-
-class UpgradeBracketsCommand(sublime_plugin.TextCommand):
-    """docstring for UpgradeBracketsCommand"""
-    brackets = [('(', ')'), ('[', ']'), ('ltext', 'rtext')]
-
-    def run(self, edit):
-        region = self.view.sel()[0]   # current cursor position (assuming no text selection)
-        global_string = self.view.line(region)
-        l, brl = find_left_bracket(global_string[:region.begin()])
-        r, brr = find_left_bracket(global_string[region.end():])
-        if (l > 0) and (r > 0) and (brl == brr):
-            self.view.insert(edit, l, " \\left ")
-            self.view.insert(edit, r, " \\right ")
-
-    def find_right_bracket(s):
-        global brackets
-        i = 0
-        right_brackets = [pairs[1] for pairs in brackets]
-        while i < len(s):
-            for br in right_brackets:
-                if s[i:].startswith(br):
-                    return (i, br)
-            i += 1
-        return -1, False
-
-    def find_left_bracket(s):
-        global brackets
-        i = len(s)
-        left_brackets = [pairs[0] for pairs in brackets]
-        while i > -1:
-            for br in left_brackets:
-                if s[:i].endswith(br):
-                    return (i - len(br), br)
-            i -= 1
-        return -1, False
